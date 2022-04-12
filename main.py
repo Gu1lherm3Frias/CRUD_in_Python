e745a7fbcdd9a9024d01f7f0b49935ddc52c5e2a
Guilherme <guilherme.frias.o.t3@gmail.com>
2022-04-12 00:55:51 -0300

    dept_emp table added

--- main.py
+++ main.py
@@ -30,6 +30,22 @@ TABLES['salaries'] = (
     " `salary` int(11) NOT NULL,"
     " `from_date` date NOT NULL"
 )
+
+TABLES['dept_emp'] = (
+    "CREATE TABLE `dept_emp` ("
+    " `emp_no` int(11) NOT NULL,"
+    " `dept_no` char(4) NOT NULL,"
+    " `from_date` date NOT NULL,"
+    " `to_date` date NOT NULL,"
+    " PRIMARY KEY (`emp_no`, `dept_no`), KEY `emp_no` (`emp_no`),"
+    " KEY `dept_no` (`dept_no`),"
+    " CONSTRAINT `dept_emp_ibfk_1` FOREIGN KEY (`emp_no`)"
+    " REFERENCES `employees` (`emp_no`) ON DELETE CASCADE,"
+    " CONSTRAINT `dept_emp_ibfk2` FOREIGN KEY (`dept_no`)"
+    " REFERENCES `departments` (`dept_no`) ON DELETE CASCADE"
+    ") ENGINE=InnoDB"
+)
+
 def main():
     cnx = mysql.connector.connect(user='scott', password='password', host='127.0.0.1', database='employes')
     cnx.close()
